1e52da50e65a19f40f64edeae03318921ee1b863
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-06-29 09:40:39 +0600

    feat: implement url list reader

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import logging
+from collections.abc import Generator
+
+def read_url_list(path : str) -> Generator[str, None, None]:
+    try: 
+        with open(file=path, mode='r', encoding='utf-8') as file:
+            for line in file:
+                yield line.rstrip("\n")
+    except FileNotFoundError as e:
+        logging.error(f"File not found: {e}")
+        raise
+    except OSError as e:
+        logging.error(f"OS error: {e}")
+        raise
+
 def main():
-    print("Hello from async-batch-url-health-checker!")
+    FILE = "url_list.txt"
+    generator = read_url_list(FILE)
+
+    for url in generator:
+        print(url)
 
 
 if __name__ == "__main__":
